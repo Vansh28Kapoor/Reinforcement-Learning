46e1ec855a6fe983fcb329ae6138f61be712e5ec
Vansh Kapoor <103594962+Vansh28Kapoor@users.noreply.github.com>
2023-11-19 23:15:59 +0530

    Add files via upload

---
+++ Reinforcement Learning on Billiards/code/agent.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import random 
+import json
+import math
+import utils
+import time
+import config
+import numpy
+random.seed(73)
+
+class Agent:
+    def __init__(self, table_config) -> None:
+        self.table_config = table_config
+        self.prev_action = None
+        self.curr_iter = 0
+        self.state_dict = {}
+        self.holes =[]
+        self.ns = utils.NextState()
+
+
+    def set_holes(self, holes_x, holes_y, radius):
+        for x in holes_x:
+            for y in holes_y:
+                self.holes.append((x[0], y[0]))
+        self.ball_radius = radius
+
+
+    def action(self, ball_pos=None):
+        ## Code you agent here ##
+        ## You can access data from config.py for geometry of the table, configuration of the levels, etc.
+        ## You are NOT allowed to change the variables of config.py (we will fetch variables from a different file during evaluation)
+        ## Do not use any library other than those that are already imported.
+        ## Try out different ideas and have fun!
+        return (2*random.random() - 1, random.random())
